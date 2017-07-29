109783f5c4452ff65030e29207a89f7bb0942a11
MCampbell08 <WaffleDefend29@gmail.com>
2017-07-29 01:36:44 -0600

    My shit

---
+++ EmployeeRecognition/EmployeeRecognition_Lib/Enums/Departments.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeRecognition_Lib.Enums
+{
+    public enum Departments
+    {
+        Production,
+        Marketing,
+        Human_Resource_Management,
+        Purchasing,
+        Accounting_And_Finance,
+        Research_And_Development
+    }
+}
