0ed54a8aa3634b1e787744b5d60f8defe4924d0a
MCampbell08 <WaffleDefend29@gmail.com>
2017-07-29 20:47:05 -0600

    Changes

---
+++ EmpRec/EmpRec/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeRecognition.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //if (User.Identity.IsAuthenticated)
+            //{
+                return View();
+            //}
+            //return RedirectToAction("Login", "Account");
+        }
+
+        public IActionResult Review()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Drawing()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
