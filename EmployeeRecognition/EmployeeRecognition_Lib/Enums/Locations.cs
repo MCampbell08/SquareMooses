4026e5cbde14ad3b818a34d8c5db933b46cd2bc5
MCampbell08 <WaffleDefend29@gmail.com>
2017-07-29 22:36:21 -0600

    Switching Back

---
+++ EmployeeRecognition/EmployeeRecognition_Lib/Enums/Locations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeRecognition_Lib.Enums
+{
+    public enum Locations
+    {
+        Provo_UT,
+        Draper_UT,
+        SanClemente_CA
+    }
+}
---
+++ EmployeeRecognition/EmployeeRecognition_Lib/Models/Submission.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EmployeeRecognition_Lib.Enums;
+
+namespace EmployeeRecognition_Lib.Models
+{
+    public class Submission
+    {
+        public int SubmissionID { get; set; }
+        public string Recipient { get; set; }
+        public Dictionary<string, bool> WhyHustler{ get; set; }
+        public Departments Department { get; set; }
+        public Locations Location { get; set; }
+        public string Date { get; set; }
+        public string Submitter { get; set; }
+        public string SubmitterEmail { get; set; }
+        public string RecipientEmail { get; set; }
+        public string Reason { get; set; }
+
+    }
+}
